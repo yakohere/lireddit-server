eb3e7e7be7cdf99681972456b0fe692669e495d8
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-09-06 01:28:24 +0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!')
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "lireddit",
+    user: "",
+    password: "",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+
+  const post = orm.em.create(Post, { title: "My first post" });
+  await orm.em.persistAndFlush(post);
+};
+
+main();
