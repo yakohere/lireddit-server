43ca6b889ff15af9801f1769c398dbc9ea62b0b8
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-09-06 11:32:48 +0500

    reading data from database using graphql

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-// import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 require("dotenv").config();
 
@@ -15,11 +16,12 @@ const main = async () => {
 
   const app = express();
 
-  const apolloServer = new ApolloServer({ 
+  const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
   apolloServer.applyMiddleware({ app });
